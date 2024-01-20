0294a160dd31a39a76f06599706458ddbf766f95
Pragyashah23 <147946980+Pragyashah23@users.noreply.github.com>
2024-01-20 02:33:31 -0600

    Add files via upload

---
+++ TradeData.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE test;
+USE test;
+CREATE TABLE stocks(TradeDate CHAR(10),
+					SPY double, 
+                    GLD double,
+                    AMZN double,
+                    GOOG double,
+                    KPIT double,
+                    GILD double,
+                    MPC double);
+ SELECT * FROM stocks;
+
+ 
+
+
+ 
+ 
+
+ 
+ 
+ 
